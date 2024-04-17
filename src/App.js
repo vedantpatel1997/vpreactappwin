c1137e93a8af7264b51617d4feb27a43137a78f8
vedantpatel1997 <vedantp9@gmail.com>
2024-04-17 15:45:52 -0600

    Initial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>REACT APP</h1>
+        <p>Hosted on Windows</p>
+        <p>VPReactAppWin</p>
       </header>
     </div>
   );
